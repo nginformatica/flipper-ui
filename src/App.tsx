e9485659a1f2f142a7127d224368143f7331971a
unknown <william.rozin@NG035-PC.ng.local>
2018-08-29 20:09:31 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Avatar from './components/Avatar'
 import Button from './components/Button'
+import Header from './components/Header'
 import Paper from './components/Paper'
 import TextField from './components/TextField'
 
 const App = (
-    <Paper padding={ 24 }>
-        <Avatar>A</Avatar>
-        <Button variant='outlined'>Click me</Button>
-        <div>
-            <TextField />
-        </div>
-    </Paper>
+    <div>
+        <Header>Flipper-UI</Header>
+        <Paper padding={ 24 }>
+            <Avatar>A</Avatar>
+            <Button variant='outlined'>Click me</Button>
+            <div>
+                <TextField />
+            </div>
+        </Paper>
+    </div>
 )
 
 ReactDOM.render(App, document.getElementById('root'))
