70b1f075b569893637f5283963c458168079991b
William Rozin Gaspar <william.rozin@ngi.com.br>
2018-09-04 14:27:00 -0300

    remove dist

--- dist/core/Box.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { IDefault } from './Advertise';
-interface IProps extends IDefault {
-    minHeight?: number;
-    children?: React.ReactNode;
-}
-declare const Box: React.SFC<IProps>;
-export default Box;
--- dist/core/List.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { IDefault } from './Advertise';
-interface IProps extends IDefault {
-    title?: string;
-    children?: React.ReactNode;
-}
-declare const List: React.SFC<IProps>;
-export default List;
