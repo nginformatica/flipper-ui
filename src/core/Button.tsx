111f6e287faec1b53b624272c00a5fb601b62370
osmarpetry <osmarpetry@gmail.com>
2018-10-09 11:36:32 -0300

    corrigido tslint

--- src/core/Button.tsx
+++ src/core/Button.tsx
@@ -1,7 +1,7 @@
 import { Button as MuiButton } from '@material-ui/core'
 import React from 'react'
+import styled from 'styled-components'
 import { IDefault } from './Advertise'
-import styled from 'styled-components';
 
 interface IProps extends IDefault {
     mini?: boolean
@@ -17,13 +17,16 @@ interface IProps extends IDefault {
 
 const Button: React.SFC<IProps> = ({ children, margin, padding, style = {}, variant, ...otherProps }) =>
     <MuiButton
-        {...otherProps}
-        variant={variant === 'dashed' ? 'outlined' : variant}
-        style={{ margin, padding, ...style }}>
-        {children}
+        { ...otherProps }
+        variant={ variant === 'dashed' ? 'outlined' : variant }
+        style={ { margin, padding, ...style } }>
+        { children }
     </MuiButton>
 
-const styleDashed = styled(Button)`
-    border-style: ${props => props.variant === 'dashed' ? 'dashed !important;' : 'initial'}
+const ButtonStyled = styled(Button)`
+    border-style: ${props => props.variant === 'dashed'
+        ? 'dashed !important'
+        : 'initial'
+    }
 `
-export default styleDashed
+export default ButtonStyled
