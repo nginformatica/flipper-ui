56604eb31012788a90f5ae91a9e669ab6eb86345
osmarpetry <osmarpetry@gmail.com>
2018-10-09 10:55:39 -0300

    implementado dashed button

--- src/core/Button.tsx
+++ src/core/Button.tsx
@@ -1,6 +1,7 @@
 import { Button as MuiButton } from '@material-ui/core'
 import React from 'react'
 import { IDefault } from './Advertise'
+import styled from 'styled-components';
 
 interface IProps extends IDefault {
     mini?: boolean
@@ -9,16 +10,20 @@ interface IProps extends IDefault {
     size?: 'small' | 'medium' | 'large'
     href?: string
     fullWidth?: boolean
-    variant?: 'text' | 'flat' | 'outlined' | 'contained' | 'raised' | 'fab' | 'extendedFab'
+    variant?: 'text' | 'flat' | 'outlined' | 'contained' | 'raised' | 'fab' | 'extendedFab' | 'dashed'
     children?: React.ReactNode
     onClick?: () => void
 }
 
-const Button: React.SFC<IProps> = ({ children, margin, padding, style = {}, ...otherProps })  =>
+const Button: React.SFC<IProps> = ({ children, margin, padding, style = {}, variant, ...otherProps }) =>
     <MuiButton
-        { ...otherProps }
-        style={ { margin, padding, ...style } }>
-        { children }
+        {...otherProps}
+        variant={variant === 'dashed' ? 'outlined' : variant}
+        style={{ margin, padding, ...style }}>
+        {children}
     </MuiButton>
 
-export default Button
+const styleDashed = styled(Button)`
+    border-style: ${props => props.variant === 'dashed' ? 'dashed !important;' : 'initial'}
+`
+export default styleDashed
