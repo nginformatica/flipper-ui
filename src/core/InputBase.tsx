3e8871e7021228b16241d0b7d5d9882c07b0a8e5
osmarpetry <osmarpetry@gmail.com>
2019-01-22 16:43:34 -0200

    props is there  🤻

--- src/core/InputBase.tsx
+++ src/core/InputBase.tsx
@@ -1,9 +1,30 @@
 import { InputBase as MuiInputBase } from '@material-ui/core'
-import React, { SFC } from 'react'
+import React, { ChangeEvent, ReactNode, SFC } from 'react'
 import { IDefault } from './Advertise'
 
 export interface IProps extends IDefault {
-    children: React.ReactNode
+    children: ReactNode
+    autoComplete?: string
+    autoFocus?: boolean
+    classes?: object
+    defaultValue?: string | number
+    disabled?: boolean
+    endAdornment?: ReactNode
+    error?: boolean
+    fullWidth?: boolean
+    id?: string
+    inputComponent?: string
+    inputProps?: object
+    multiline?: boolean
+    onChange?: (event: ChangeEvent<HTMLInputElement>) => void
+    placeholder?: string
+    readonly?: boolean
+    required?: boolean
+    rows?: string | number
+    rowsMax?: string | number
+    startAdornment?: ReactNode
+    type?: string
+    value?: string | number | boolean
 }
 
 const InputAdornment: SFC<IProps> = ({ children, margin, padding, style = {}, ...otherProps }) =>
