de7be4eec54b9427ce522d5c5489b4d41e4c41e8
leonardo.rigon <leonardo.rigon@ngi.com.br>
2019-02-18 11:58:10 -0300

    props created, TODO: define required props

---
+++ src/core/DatePicker.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, SFC } from 'react'
+import { DatePicker as MuiDatePicker } from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
+interface IDateField {
+    showTimeSelect: boolean
+    timeFormat: string
+    timeIntervals: number
+    value: string
+    openToDate: string
+    customInput: ReactNode
+    onChange: ReactNode
+    locale: string
+    placeholderText: string
+}
+
+const DatePicker: SFC<IDateField> = props =>
+    <MuiDatePicker { ...props } />
+
+export default DatePicker
