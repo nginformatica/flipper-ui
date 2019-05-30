47e1b65808b8e6bba12c570c386ff4985a11eb90
William Rozin Gaspar <william.rozin@ngi.com.br>
2019-05-30 11:06:25 -0300

    allow to set only date and time formats too

--- src/core/DateTime.tsx
+++ src/core/DateTime.tsx
@@ -1,92 +1,88 @@
-import React, { FC, ReactNode } from 'react'
+import React, { FC } from 'react'
 import {
+    KeyboardTimePicker,
+    KeyboardDatePicker,
     KeyboardDateTimePicker,
     MuiPickersUtilsProvider
 } from '@material-ui/pickers'
 import DateFnsUtils from '@date-io/date-fns'
 import { IDefault } from './Advertise'
-import { PopoverProps } from '@material-ui/core/Popover'
-import { IconButtonProps } from '@material-ui/core/IconButton'
 import { IClasses, styles } from './TextField'
-import { InputLabelProps } from '@material-ui/core/InputLabel'
-import { InputProps } from '@material-ui/core/Input'
 import { withStyles } from '@material-ui/styles'
-import { InputAdornmentProps } from '@material-ui/core/InputAdornment'
-import { DialogProps } from '@material-ui/core/Dialog'
+import { KeyboardDatePickerProps } from '@material-ui/pickers/DatePicker'
+import { KeyboardDateTimePickerProps } from '@material-ui/pickers/DateTimePicker'
+import { KeyboardTimePickerProps } from '@material-ui/pickers/TimePicker'
 
 interface IProps extends IDefault {
-    autoFocus?: boolean
-    id?: string
-    name?: string
-    variant?: 'dialog' | 'inline'
-    inputVariant?: 'standard' | 'outlined' | 'filled'
-    ampm?: boolean
-    label?: string
-    value: Date
-    mask?: string
-    markChar?: string
-    maxDate?: Date
-    minDate?: Date
-    format?: string
-    maxDateMessage?: ReactNode
-    minDateMessage?: ReactNode
-    minutesStep?: number
-    PopoverProps?: Partial<PopoverProps>
-    refuse?: RegExp
-    rightArrowButtonProps?: Partial<IconButtonProps>
-    allowKeyboardControl?: boolean
-    InputLabelProps?: InputLabelProps
-    InputAdornmentProps?: Partial<InputAdornmentProps>
-    InputProps?: InputProps
-    DialogProps?: DialogProps
-    okLabel?: ReactNode
-    cancelLabel?: ReactNode
-    views?: Array<'date' | 'year' | 'month' | 'hours' | 'minutes'>
-    onChange: ((date: Date, value: string) => void) & ((date: Date) => void)
-    shouldDisableDate?(date: Date): boolean
+    type?: 'date' | 'time' | 'datetime'
 }
 
-const DateTime: FC<IProps & IClasses> = ({
+type TProps =
+    & IProps
+    & IClasses
+    & KeyboardDatePickerProps
+    & KeyboardDateTimePickerProps
+    & KeyboardTimePickerProps
+
+const DateTime: FC<TProps> = ({
     padding,
     margin,
     style,
-    format = 'dd/MM/yyyy HH:mm',
+    format,
     variant = 'inline',
     inputVariant = 'outlined',
     classes,
-    ...props
-}) =>
-    <MuiPickersUtilsProvider utils={ DateFnsUtils }>
-        <KeyboardDateTimePicker
-            format={ format }
-            variant={ variant }
-            inputVariant={ inputVariant }
-            style={ { margin, padding, ...style } }
-            InputAdornmentProps={ {
-                style: { width: '32px' },
-                ...props.InputAdornmentProps
-            } }
-            InputLabelProps={ {
-                classes: {
-                    outlined: inputVariant === 'outlined'
-                        ? classes.outlinedLabel
-                        : ''
-                },
-                ...props.InputLabelProps
-            } }
-            InputProps={ {
-                classes: {
-                    input: inputVariant === 'outlined'
-                        ? classes.outlinedInput
-                        : '',
-                    multiline: inputVariant === 'outlined'
-                        ? classes.outlinedMultiline
-                        : ''
-                },
-                ...props.InputProps
-            } }
-            { ...props }
-        />
-    </MuiPickersUtilsProvider>
+    type = 'datetime',
+    ...otherProps
+}) => {
+    const defaultFormats = {
+        date: 'dd/MM/yyyy',
+        time: 'HH:mm',
+        datetime: 'dd/MM/yyyy HH:mm'
+    }
+
+    const fieldProps = {
+        format: format ? format : defaultFormats[type],
+        variant,
+        inputVariant,
+        style: { margin, padding, ...style },
+        InputAdornmentProps: {
+            style: { width: '32px' },
+            ...otherProps.InputAdornmentProps
+        },
+        InputLabelProps: {
+            classes: {
+                outlined: inputVariant === 'outlined'
+                    ? classes.outlinedLabel
+                    : ''
+            },
+            ...otherProps.InputLabelProps
+        },
+        InputProps: {
+            classes: {
+                input: inputVariant === 'outlined'
+                    ? classes.outlinedInput
+                    : '',
+                multiline: inputVariant === 'outlined'
+                    ? classes.outlinedMultiline
+                    : ''
+            },
+            ...otherProps.InputProps
+        },
+        ...otherProps
+    }
+
+    const renderDatePicker = () => <KeyboardDatePicker { ...fieldProps } />
+    const renderTimePicker = () => <KeyboardTimePicker { ...fieldProps } />
+    const renderDateTimePicker = () => <KeyboardDateTimePicker { ...fieldProps } />
+
+    return (
+        <MuiPickersUtilsProvider utils={ DateFnsUtils }>
+            { type === 'date' && renderDatePicker() }
+            { type === 'time' && renderTimePicker() }
+            { type === 'datetime' && renderDateTimePicker() }
+        </MuiPickersUtilsProvider>
+    )
+}
 
 export default withStyles(styles)(DateTime)
