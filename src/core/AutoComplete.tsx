1e4d5495c7f205cb0791389df82add2adb9146b9
William Rozin Gaspar <william.rozin@ngi.com.br>
2019-05-31 18:17:44 -0300

    new autocomplete

--- src/core/AutoComplete.tsx
+++ src/core/AutoComplete.tsx
@@ -1,42 +1,30 @@
-import Downshift from 'downshift'
-import {
-    contains,
-    filter,
-    is,
-    isNil,
-    pipe,
-    propEq,
-    propOr,
-    toLower,
-    uniq,
-    unless,
-    when
-} from 'ramda'
 import React, {
     cloneElement,
-    Component,
     FocusEvent,
     Fragment,
-    ReactNode
+    ReactNode,
+    FC,
+    useState,
+    useRef,
+    ChangeEvent,
+    KeyboardEvent
 } from 'react'
 import Paper from './Paper'
 
 interface IProps {
-    data: object[] | string[]
-    value: string
+    data: TSelected[]
+    value: TSelected
     defaultValue?: string
-    inputElement: JSX.Element
+    inputElement: React.ReactElement
     InputProps?: object
     defaultIsOpen?: boolean
     openOnFocus?: boolean
-    selected?: ISelected | ISelected[]
     renderSuggestion: (
         suggestion: string | object,
         itemProps: object,
         selected: boolean
     ) => ReactNode
-    onChange?: (value: string | ISelected) => void
-    onSelect?: (value: ISelected) => void
+    onChange?: (value: TSelected) => void
     onFocus?: (event: FocusEvent<HTMLInputElement>) => void
     onBlur?: (event: FocusEvent<HTMLInputElement>) => void
 }
@@ -45,15 +33,62 @@ interface ISelected {
     label: string
     value: string
     type?: string
+    subheader?: string
 }
 
-class AutoComplete extends Component<IProps> {
-    public autocomplete: HTMLInputElement | null = null
+type TSelected = ISelected | string
 
-    private getPaperPosition(inputValue: string | null) {
-        if (this.autocomplete !== null) {
-            const height = this.getSuggestions(inputValue).length * 48
-            const { top } = this.autocomplete.getBoundingClientRect()
+const AutoComplete: FC<IProps> = props => {
+    const initialValue = typeof props.value === 'object'
+        ? props.value.label
+        : props.value || ''
+
+    const inputRef = useRef<HTMLInputElement>(null)
+    const [value, setValue] = useState<string>(initialValue)
+    const [highlighted, setHighlighted] = useState(0)
+    const [open, setOpen] = useState(!!props.defaultIsOpen)
+
+    const handleSelect = (item: TSelected) => {
+        if (typeof item === 'object' && item.subheader) {
+            return
+        }
+
+        setValue(typeof item === 'object' ? item.label : item || '')
+        setOpen(false)
+
+        if (props.onChange) {
+            props.onChange(item)
+        }
+    }
+
+    const getSuggestions = (): TSelected[] => {
+        if (props.openOnFocus && !value) {
+            return props.data
+        }
+
+        const items = value
+            ? props.data
+            : []
+
+        return items
+            .filter(item => {
+                if (typeof item === 'object') {
+                    if (item.subheader) {
+                        return true
+                    }
+
+                    return item.label.toLowerCase().includes(value.toLowerCase())
+                }
+
+                return item.toLowerCase().includes(value.toLowerCase())
+            })
+    }
+
+    const getPaperPosition = () => {
+
+        if (inputRef.current !== null) {
+            const height = getSuggestions().length * 48
+            const { top } = inputRef.current.getBoundingClientRect()
 
             if ((top + height) > window.innerHeight) {
                 return 'above'
@@ -63,72 +98,56 @@ class AutoComplete extends Component<IProps> {
         return 'below'
     }
 
-    public getSuggestions(inputValue: string | null): Array<string | object> {
-        if (this.props.openOnFocus && !inputValue) {
-            return this.props.data
-        }
-
-        const items = inputValue
-            ? this.props.data
-            : []
-
-        return uniq(
-            filter<string | object>(
-                unless(
-                    propEq('subheader', true),
-                    pipe(
-                        when(is(Object), propOr('', 'label')),
-                        toLower,
-                        contains(toLower(inputValue || ''))
-                    )
-                ),
-                items
-            )
-        )
-    }
-
-    public handleSelect(value: ISelected) {
-        if (this.props.onSelect) {
-            this.props.onSelect(value)
-        } else {
-            this.handleChange(value)
-        }
-    }
-
-    public handleChange(value: string | ISelected) {
-        if (!isNil(value) && this.props.onChange) {
-            this.props.onChange(value)
-        }
-    }
-
-    public renderInput(props: {
-        inputProps: object,
-        onFocus: (event: FocusEvent<HTMLInputElement>) => void
-    }) {
-        return cloneElement(this.props.inputElement, {
-            InputProps: {
-                ...props,
-                ...this.props.InputProps
-            },
-            inputProps: {
-                ref: (self: HTMLInputElement) => {
-                    this.autocomplete = self
-                }
+    const getItemProps = (item: TSelected) => {
+        return {
+            onClick: (event: Event) => {
+                event.preventDefault()
+                event.stopPropagation()
+                handleSelect(item)
             }
-        })
+        }
     }
 
-    public renderSugestionPaper({
-        inputValue,
-        getItemProps,
-        highlightedIndex
-    }) {
+    const handleFocus = (event: FocusEvent<HTMLInputElement>) => {
+        if (props.openOnFocus) {
+            setOpen(true)
+        }
+
+        if (props.onFocus) {
+            props.onFocus(event)
+        }
+    }
+
+    const handleBlur = (event: FocusEvent<HTMLInputElement>) => {
+        setTimeout(() => setOpen(false), 200)
+
+        if (props.onBlur) {
+            props.onBlur(event)
+        }
+    }
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value)
+        setHighlighted(0)
+
+        if (props.onChange) {
+            props.onChange(value)
+        }
+
+        if (getSuggestions().length > 0) {
+            setOpen(true)
+        } else {
+            setOpen(false)
+        }
+    }
+
+    const renderSuggestions = () => {
         const paperStyle = {
             position: 'absolute' as 'absolute',
-            width: this.autocomplete ? this.autocomplete.offsetWidth : 256,
-            bottom: this.getPaperPosition(inputValue) === 'above'
-                && this.autocomplete
-                ? this.autocomplete.offsetHeight + 1
+            width: inputRef.current ? inputRef.current.offsetWidth : 256,
+            bottom: getPaperPosition() === 'above'
+                    && inputRef.current
+                ? inputRef.current.getBoundingClientRect().height + 1
                 : undefined,
             zIndex: 1099
         }
@@ -136,13 +155,13 @@ class AutoComplete extends Component<IProps> {
         return (
             <Paper square style={ paperStyle }>
                 {
-                    this.getSuggestions(inputValue).map((suggestion, index) =>
+                    getSuggestions().map((suggestion, index) =>
                         <Fragment key={ index }>
                             {
-                                this.props.renderSuggestion(
+                                props.renderSuggestion(
                                     suggestion,
-                                    getItemProps({ item: suggestion }),
-                                    highlightedIndex === index
+                                    getItemProps(suggestion),
+                                    highlighted === index
                                 )
                             }
                         </Fragment>
@@ -152,63 +171,38 @@ class AutoComplete extends Component<IProps> {
         )
     }
 
-    public render() {
-        return (
-            <Downshift
-                inputValue={ this.props.value }
-                defaultInputValue={ this.props.defaultValue }
-                selectedItem={ this.props.selected }
-                defaultIsOpen={ this.props.defaultIsOpen }
-                itemToString={ item => is(Object, item) ? item.label : item }
-                onSelect={ this.handleSelect.bind(this) }
-                onInputValueChange={ this.props.onChange }>
-                {
-                    ({
-                        isOpen,
-                        getInputProps,
-                        inputValue,
-                        getItemProps,
-                        highlightedIndex,
-                        openMenu
-                    }) =>
-                        <div
-                            style={
-                                this.getPaperPosition(inputValue) === 'above'
-                                    ? {
-                                        display: 'flex',
-                                        position: 'relative' as 'relative',
-                                        flexFlow: 'column-reverse'
-                                    }
-                                    : {}
-                            }>
-                            {
-                                this.renderInput(getInputProps({
-                                    onBlur: this.props.onBlur,
-                                    onFocus:
-                                        (event: FocusEvent<HTMLInputElement>) => {
-                                            if (this.props.onFocus) {
-                                                this.props.onFocus(event)
-                                            }
-
-                                            if (this.props.openOnFocus) {
-                                                openMenu()
-                                            }
-                                        },
-                                    value: inputValue || ''
-                                }))
-                            }
-                            {
-                                isOpen && this.renderSugestionPaper({
-                                    inputValue,
-                                    getItemProps,
-                                    highlightedIndex
-                                })
-                            }
-                        </div>
-                }
-            </Downshift>
-        )
+    const handleNavigate = (event: KeyboardEvent) => {
+        if (event.key === 'ArrowDown' && highlighted < getSuggestions().length - 1) {
+            setHighlighted(highlighted + 1)
+        } else if (event.key === 'ArrowUp' && highlighted > 0) {
+            setHighlighted(highlighted - 1)
+        } else if (event.key === 'Enter') {
+            const item = getSuggestions()[highlighted]
+            handleSelect(item)
+        }
     }
+
+    const renderInput = () =>
+        cloneElement(props.inputElement, {
+            value,
+            onChange: handleChange,
+            onFocus: handleFocus,
+            onBlur: handleBlur,
+            onKeyDown: handleNavigate,
+            InputProps: {
+                ...props.InputProps
+            },
+            inputProps: {
+                ref: inputRef
+            }
+        })
+
+    return (
+        <div style={ { position: 'relative' } }>
+            { renderInput() }
+            { open && renderSuggestions() }
+        </div>
+    )
 }
 
 export default AutoComplete
