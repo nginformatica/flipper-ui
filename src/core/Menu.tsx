9bb75743410cdc6709ee07991ce18bbd236e8e2b
Jeova Gomes <jeova.gomes@ngi.com.br>
2019-10-09 13:32:50 -0300

    fix(menu): remove unnecessary interface

--- src/core/Menu.tsx
+++ src/core/Menu.tsx
@@ -2,9 +2,8 @@ import { Menu as MuiMenu } from '@material-ui/core'
 import React, { FC } from 'react'
 import { IDefault } from './Advertise'
 import { MenuProps } from '@material-ui/core/Menu'
-import { PopoverProps } from '@material-ui/core/Popover'
 
-interface IProps extends IDefault, PopoverProps, MenuProps {
+interface IProps extends IDefault, MenuProps {
     open: boolean
     anchorEl?: HTMLElement
     withWrapper?: boolean
