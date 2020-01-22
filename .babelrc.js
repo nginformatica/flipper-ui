1a3befef233ab5c69112196e49b43b92d0534cf4
JeovaZERO <jeovapereira2000@live.com>
2020-01-22 17:38:03 +0000

    chore(deps): update dependencies

--- .babelrc.js
+++ .babelrc.js
@@ -108,8 +108,6 @@ module.exports = {
         },
       }
     ],
-    "@babel/proposal-class-properties",
-    "@babel/proposal-object-rest-spread",
     "@babel/plugin-transform-runtime",
   ],
   "presets": [
