1d71dd3aae3b0d3fceeaf4ecc38ac960ce169ab5
JeovaZERO <jeovapereira2000@live.com>
2020-01-22 17:40:33 +0000

    chore(TextField): change the outlinedIcon position

--- src/core/TextField.tsx
+++ src/core/TextField.tsx
@@ -52,8 +52,8 @@ export const useStyles = makeStyles({
     outlinedMultiline: {
         padding: '0px'
     },
-    select: {
-        paddingRight: '34px'
+    iconOutlined: {
+        right: '2px'
     }
 })
 
@@ -67,6 +67,7 @@ const TextField: FC<TProps> = ({
     variant = 'outlined',
     InputLabelProps = {},
     InputProps = {},
+    SelectProps = {},
     autoComplete='off',
     ...otherProps
 }) => {
@@ -92,10 +93,15 @@ const TextField: FC<TProps> = ({
                 classes: {
                     input: variant === 'outlined' ? classes.outlinedInput : '',
                     multiline:
-                        variant === 'outlined' ? classes.outlinedMultiline : '',
-                    inputSelect: classes.select
+                        variant === 'outlined' ? classes.outlinedMultiline : ''
                 },
                 ...InputProps
+            } }
+            SelectProps={ {
+                classes: {
+                    iconOutlined: classes.iconOutlined
+                },
+                ...SelectProps
             } }
             { ...otherProps }
         />
