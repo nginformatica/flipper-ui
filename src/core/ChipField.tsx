945a77e0830c0ee52db447b1cfe749da641da563
leonardo.rigon <leonardo.rigon@ngi.com.br>
2021-01-15 18:17:25 -0300

    chore(chip-field): add chip-field library, component and example

---
+++ src/core/ChipField.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ChipInput from 'material-ui-chip-input'
+import { InputLabelProps } from '@material-ui/core'
+
+interface IProps<T extends TChipValues> {
+  allowDuplicates?: boolean
+  values: T[]
+  variant?: 'outlined' | 'standard' | 'filled'
+  disabled?: boolean
+  fullWidth?: boolean
+  fullWidthInput?: boolean
+  InputLabelProps?: InputLabelProps
+  label?: React.ReactNode
+  placeholder?: string
+  readOnly?: boolean
+  onAdd: (values: T[]) => void;
+  onDelete: (value: T, index: number) => void;
+}
+
+export type TChipValues = {
+  value: string | number
+}
+
+const ChipField = <T extends TChipValues>(props: IProps<T>) => {
+    const values = props.values.map(item => item.value)
+
+    return (
+        <ChipInput
+            { ...props }
+            alwaysShowPlaceholder
+            value={ values }
+        />
+    )
+}
+
+export default ChipField
