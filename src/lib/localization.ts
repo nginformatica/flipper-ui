1195edde16651e4a01e9936f94fd00c70c074db8
leorigon <rigonnleo@gmail.com>
2021-07-21 18:12:48 -0300

    chore(editable-table): add localization file with the getLocalization function

---
+++ src/lib/localization.ts
@@ -0,0 +1,30 @@
+import ptBRLocale from 'date-fns/locale/pt-BR'
+
+export const getLocalization = (title: string = '') => ({
+    body: {
+        dateTimePickerLocalization: { locale: ptBRLocale },
+        emptyDataSourceMessage: 'Não há dados para serem exibidos no momento',
+        editRow: {
+            saveTooltip: 'Salvar',
+            cancelTooltip: 'Cancelar',
+            deleteText:
+                `Você tem certeza que deseja excluir esse ${title ?? 'item'}?`
+        },
+        addTooltip: `Adicionar ${title}`,
+        deleteTooltip: `Remover ${title}`,
+        editTooltip: `Editar ${title}`
+    },
+    header: { actions: '' },
+    pagination: {
+        firstTooltip: 'Primeira',
+        firstAriaLabel: 'Primeira',
+        previousTooltip: 'Anterior',
+        previousAriaLabel: 'Anterior',
+        nextTooltip: 'Próxima',
+        nextAriaLabel: 'Próxima',
+        lastTooltip: 'Ultima',
+        lastAriaLabel: 'Ulima',
+        labelRowsSelect: 'Linhas',
+        labelDisplayedRows: '{from}-{to} de {count}'
+    }
+})
