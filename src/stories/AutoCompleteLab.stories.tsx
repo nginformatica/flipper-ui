1c1ea2d23c73971c32e2239b555fa195df45fe26
Henrique Souza <henrique.asouza@outlook.com>
2021-11-05 14:57:04 -0300

    docs: add autocompletelab.stories

---
+++ src/stories/AutoCompleteLab.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { ComponentMeta } from '@storybook/react'
+import AutoComplete from '../core/AutoComplete'
+import TextField from '../core/TextField'
+import { Autocomplete } from '@material-ui/lab'
+
+export default {
+    title: 'Testing/AutoCompleteLab',
+    component: AutoComplete
+} as ComponentMeta<typeof AutoComplete>
+
+export const Default = () => (
+    <Autocomplete
+        options={ [
+            { label: 'Anemone', value: 'forget-me-not' },
+            { label: 'Forget me not', value: 'anemone' },
+            { label: 'Snapdragon', value: 'snapdragon' },
+            { label: 'Sunflower', value: 'sunflower' },
+            { label: 'Rose', value: 'rose' }
+        ] }
+        getOptionLabel={ option => option.label }
+        style={ { width: 300 } }
+        renderInput={ props => (
+            <TextField
+                { ...props }
+                label='Flowers'
+                placeholder='Flowers'
+                variant='outlined'
+                fullWidth
+            />
+        ) }
+        onChange={ () => null }
+    />
+)
