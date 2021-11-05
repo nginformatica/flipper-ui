508e32dea9af017dbc7ff45f3ada46087fb227fb
Henrique Souza <henrique.asouza@outlook.com>
2021-11-05 14:54:32 -0300

    docs: add drawer.stories

---
+++ src/stories/Drawer.stories.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { ComponentMeta } from '@storybook/react'
+import Drawer from '../core/Drawer'
+import List from '../core/List'
+import ListItem from '../core/ListItem'
+import { Backup as IconBackup } from '../icons'
+import Button from '../core/Button'
+
+export default {
+    title: 'Test/Drawer',
+    component: Drawer
+} as ComponentMeta<typeof Drawer>
+
+export const Default = () => {
+    const [open, SetOpen] = useState(false)
+
+    const handleClick = () => {
+        SetOpen(!open)
+    }
+
+    return (
+        <>
+            <Button onClick={ handleClick } disabled={ open } >Open</Button>
+            <Drawer open={ open } onClick={ handleClick }>
+                <List>
+                    <ListItem icon={ <IconBackup /> } />
+                    <ListItem icon={ <IconBackup /> } />
+                </List>
+            </Drawer>
+        </>
+    )
+}
