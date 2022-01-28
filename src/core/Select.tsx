0c52828316f84a652c7e5ac20f01cb6735ed38bd
Henrique Souza <henrique.asouza@outlook.com>
2022-01-28 11:51:07 -0300

    chore(select): component have hasClear and onClear optional props

--- src/core/Select.tsx
+++ src/core/Select.tsx
@@ -1,38 +1,66 @@
-import { Select as MuiSelect } from '@material-ui/core'
+import {
+    IconButton,
+    InputAdornment,
+    Select as MuiSelect
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { Clear } from '@material-ui/icons'
 import React, { ChangeEvent, FC } from 'react'
 import { DefaultProps } from './types'
-import { makeStyles } from '@material-ui/core/styles'
 
 interface SelectProps extends DefaultProps {
     autoWidth?: boolean
     value?: string | number
     multiple?: boolean
     variant?: 'standard' | 'outlined' | 'filled'
+    hasClear?: boolean
+    onClear?: () => void
     onClose?: () => void
-    onChange?: (event: ChangeEvent<HTMLSelectElement | HTMLInputElement>) => void
+    onChange?: (
+        event: ChangeEvent<HTMLSelectElement | HTMLInputElement>
+    ) => void
 }
 
-const useStyles = makeStyles(() => ({
-    root: {
-        padding: '10px 24px 10px 12px'
-    },
-    iconOutlined: {
-        right: '2px'
-    }
-}))
+const iconStyle = {
+    right: '2px'
+}
+
+const renderEndAdornment = (onClear?: () => void) => {
+    return (
+        <InputAdornment position='end'>
+            <IconButton onClick={ onClear } size='small'>
+                <Clear style={ { fontSize: '15px' } } />
+            </IconButton>
+        </InputAdornment>
+    )
+}
 
 const Select: FC<SelectProps> = ({
     children,
     style = {},
     margin,
     padding,
+    hasClear,
+    onClear,
     variant = 'outlined',
     ...otherProps
 }) => {
+    const useStyles = makeStyles(() => ({
+        root: {
+            padding: `10px 24px 10px ${hasClear ? '20' : '12'}px`
+        },
+        iconOutlined: hasClear
+            ? { ...iconStyle, position: 'relative', marginLeft: '-20px' }
+            : iconStyle
+    }))
     const classes = useStyles()
+
+    const hasValue = !!otherProps.value
 
     return (
         <MuiSelect
+            { ...hasValue &&
+                hasClear && { endAdornment: renderEndAdornment(onClear) } }
             variant={ variant as 'outlined' }
             classes={ {
                 root: classes.root,
