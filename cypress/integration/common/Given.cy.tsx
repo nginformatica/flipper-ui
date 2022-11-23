3e93edb5c804fcf10218704e1f0f63e617777b58
Henrique Souza <henrique.asouza@outlook.com>
2022-11-23 18:17:42 -0300

    feat(cypress/integration/given): create I render Avatar with variant assertion

--- cypress/integration/common/Given.cy.tsx
+++ cypress/integration/common/Given.cy.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { mount } from '@cypress/react'
 import { Given } from 'cypress-cucumber-preprocessor/steps'
 import Advertise from '../../../src/core/Advertise'
+import Avatar from '../../../src/core/Avatar'
 import { generateMock } from '../../support/component'
+import { AvatarVariant } from 'support/types-interfaces-enums'
 
 Given('I render Advertise', () => {
     let author = ''
@@ -11,7 +13,25 @@ Given('I render Advertise', () => {
     generateMock('advertise-author', 'Name').then(e => {
         author = e
         generateMock('advertise-comment', 'Words')
-            .then(e => comment = e)
+            .then(e => (comment = e))
             .then(() => mount(<Advertise author={ author } comment={ comment } />))
+    })
+})
+
+Given('I render Avatar with {string} preset', (preset: AvatarVariant) => {
+    generateMock('avatar-children', 'Letter').then(mock => {
+        let params = {}
+        switch (preset) {
+            case 'primary':
+                params = Object.assign({}, { primary: true })
+                break
+
+            case 'with-children':
+                params = Object.assign({}, { children: mock, primary: true })
+                break
+            default:
+                break
+        }
+        mount(<Avatar { ...params } />)
     })
 })
