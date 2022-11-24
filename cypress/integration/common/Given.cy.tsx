e686042efa65717bd1ae2b694bd811e9bbb1ad77
Henrique Souza <henrique.asouza@outlook.com>
2022-11-24 15:51:31 -0300

    chore(cypress): move mount Box to a factory method

--- cypress/integration/common/Given.cy.tsx
+++ cypress/integration/common/Given.cy.tsx
@@ -1,35 +1,23 @@
-import { mount } from '@cypress/react'
 import { Given } from 'cypress-cucumber-preprocessor/steps'
-import { generateMock } from '../../support/component'
+import {
+    AdvertiseFactory,
+    AvatarFactory,
+    BadgeFactory,
+    BoxFactory
+} from '../../support/factories'
 import {
     AvatarVariant,
     BadgeVariant
 } from '../../support/types-interfaces-enums'
-import React from 'react'
-import { Box } from '../../../src'
-import {
-    AdvertiseFactory,
-    AvatarFactory,
-    BadgeFactory
-} from '../../support/factories'
 
 Given('I render Advertise', () => AdvertiseFactory())
 
 Given('I render Avatar with {string} preset', (preset: AvatarVariant) =>
-    AvatarFactory(preset))
+    AvatarFactory(preset)
+)
 
 Given('I render Badge with {string} preset', (preset: BadgeVariant) =>
-    BadgeFactory(preset))
+    BadgeFactory(preset)
+)
 
-Given('I render Box', () => {
-    generateMock({ value: 'box-params', type: 'BoxParams' }).then(params => {
-        if (params instanceof Object && 'minHeight' in params) {
-            generateMock({ value: 'box-children', type: 'Words' }).then(e => {
-                const child = typeof e === 'string' ? e : e.toString()
-                mount(<Box { ...params }>{ child }</Box>)
-            })
-        } else {
-            throw new Error('Invalid mock generation')
-        }
-    })
-})
+Given('I render Box', () => BoxFactory())
--- cypress/support/factories/index.tsx
+++ cypress/support/factories/index.tsx
@@ -1,3 +1,4 @@
 export * from './advertise'
 export * from './avatar'
 export * from './badge'
+export * from './box'
