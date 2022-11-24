1f93cbb949093d34081eaf4a2ec1ef0b0bff7275
Henrique Souza <henrique.asouza@outlook.com>
2022-11-24 11:12:48 -0300

    feat(cypress/support): create new types BadgeVariant, SpyCats, TMockOptions and emun MuiSelectors

--- cypress/support/types-interfaces-enums.ts
+++ cypress/support/types-interfaces-enums.ts
@@ -3,29 +3,51 @@
 import type * as H from 'history'
 
 export interface MemoryRouterProps {
-  initialEntries?: H.LocationDescriptor[]
-  initialIndex?: number
-  getUserConfirmation?: (
-    message: string,
-    callback: (ok: boolean) => void
-  ) => void
-  keyLength?: number
+    initialEntries?: H.LocationDescriptor[]
+    initialIndex?: number
+    getUserConfirmation?: (
+        message: string,
+        callback: (ok: boolean) => void
+    ) => void
+    keyLength?: number
 }
 
-export type MockTypes = 'Name' | 'Words' | 'Letter'
+export type MockTypes =
+    | 'Name'
+    | 'Words'
+    | 'Letter'
+    | 'Number'
+    | 'Numbers'
+    | 'JSXButton'
 
 export type AvatarVariant = 'default' | 'primary' | 'with-children'
+export type BadgeVariant = 'default' | 'primary' | 'secondary' | 'with-dot'
 
 export type MockCats =
-  | 'advertise-author'
-  | 'advertise-comment'
-  | 'avatar-children'
+    | 'advertise-author'
+    | 'advertise-comment'
+    | 'avatar-children'
+    | 'badge-counter'
+    | 'badge-children'
+
+export type SpyCats = 'badge-children'
 
 export interface SpyObj {
-  original: string
-  alias: string
+    original: string
+    alias: string
 }
 
 export type MockObj = SpyObj
 
 // export interface MockObj extends SpyObj {}
+
+export enum MuiSelectors {
+    BadgeDot = '.MuiBadge-dot'
+}
+
+export type TMockOptions = {
+    max?: number
+    min?: number
+    length?: number
+    onClick?: () => void
+}
