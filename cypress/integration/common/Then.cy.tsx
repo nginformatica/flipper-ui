1482844896d105041dd098a413dcc53b2cf73cce
Henrique Souza <henrique.asouza@outlook.com>
2022-11-24 14:35:17 -0300

    feat(cypress/integration/then): create I expect Box component style to match with mock assertion

--- cypress/integration/common/Then.cy.tsx
+++ cypress/integration/common/Then.cy.tsx
@@ -1,43 +1,60 @@
 import { Then } from 'cypress-cucumber-preprocessor/steps'
+import { pick } from 'ramda'
 import {
-  MockCats,
-  MuiSelectors,
-  SpyCats
+    MockCats,
+    MuiSelectors,
+    SpyCats
 } from '../../support/types-interfaces-enums'
 
 Then('I should see {string}', (text: string) =>
     cy.waitUntil(() => cy.contains(text).should('exist'))
 )
 
-Then(
-    'I expect {string} mock to exist',
-    (mockName: MockCats) => {
-      cy.getMock(mockName).then(spy => {
+Then('I expect {string} mock to exist', (mockName: MockCats) => {
+    cy.getMock(mockName).then(spy => {
         // @ts-ignore
         cy.waitUntil(() => cy.contains(spy).should('exist'))
-      })
-    }
-  )
-
-  Then('I should see {int} {string}', (quantity: number, element: string) => {
-    cy.get(element).then(elements => {
-      expect(elements.length).to.equal(quantity)
     })
-  })
+})
 
-  Then('I should not see any {string}', (element: string) => {
+Then('I should see {int} {string}', (quantity: number, element: string) => {
+    cy.get(element).then(elements => {
+        expect(elements.length).to.equal(quantity)
+    })
+})
+
+Then('I should not see any {string}', (element: string) => {
     cy.get(element).should('not.exist')
-  })
+})
 
-  Then(
+Then(
     'I expect {string} spy to have been called {int} times',
     (spyName: SpyCats, quantity: number) => {
-      cy.getSpy(spyName).should('have.been.called', quantity)
+        cy.getSpy(spyName).should('have.been.called', quantity)
     }
-  )
-
-  Then('I should see {int} BadgeDot', (quantity: number) =>
-  cy.get(MuiSelectors.BadgeDot).then(elements => {
-    expect(elements).to.length(quantity)
-  })
 )
+
+Then('I should see {int} BadgeDot', (quantity: number) =>
+    cy.get(MuiSelectors.BadgeDot).then(elements => {
+        expect(elements).to.length(quantity)
+    })
+)
+
+Then('I expect Box component style to match with mock', () => {
+    cy.getMock('box-params').then(mock => {
+        const props = pick(['name', 'id'], mock)
+        const styles = pick(['padding', 'margin', 'minHeight'], mock)
+        console.log({ props, styles })
+        for (const prop in props) {
+            cy.get('[id="box-testing-id"]')
+                .first()
+                .should('have.attr', prop)
+        }
+        for (const style in styles) {
+            cy.get('[id="box-testing-id"]')
+                .first()
+                // @ts-ignore
+                .should('have.css', style, `${styles[style]}px`)
+        }
+    })
+})
