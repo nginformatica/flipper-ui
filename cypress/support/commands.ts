0de79c034d061bdbca3f0f098cab14c1b67e9fdf
Henrique Souza <henrique.asouza@outlook.com>
2022-11-24 11:15:25 -0300

    feat(cypress/commands): create getSpy command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,9 +1,16 @@
 // / <reference types="cypress" />
-import { Mocks } from './component'
-import type { MockCats } from './types-interfaces-enums'
+import { Mocks, Spies } from './component'
+import type { MockCats, SpyCats } from './types-interfaces-enums'
 
 Cypress.Commands.add('getMock', (name: MockCats) => {
   const FALLBACK = '@unknown-spy'
 
 return cy.get(Mocks.get(name)?.alias || FALLBACK)
+})
+
+Cypress.Commands.add('getSpy', (name: SpyCats) => {
+  const FALLBACK = '@unknown-spy'
+  console.log({ name })
+
+return cy.get(Spies.get(name)?.alias || FALLBACK)
 })
