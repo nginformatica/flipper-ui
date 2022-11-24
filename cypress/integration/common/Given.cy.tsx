447254f6750b4bf846167f45a5b9d2d3fbd3baed
Henrique Souza <henrique.asouza@outlook.com>
2022-11-24 11:18:15 -0300

    feat(cypress/integration/given): create I render Avatar assertion

--- cypress/integration/common/Given.cy.tsx
+++ cypress/integration/common/Given.cy.tsx
@@ -3,23 +3,24 @@ import { mount } from '@cypress/react'
 import { Given } from 'cypress-cucumber-preprocessor/steps'
 import Advertise from '../../../src/core/Advertise'
 import Avatar from '../../../src/core/Avatar'
-import { generateMock } from '../../support/component'
-import { AvatarVariant } from 'support/types-interfaces-enums'
+import Badge, { BadgeProps } from '../../../src/core/Badge'
+import { generateMock, generateSpy } from '../../support/component'
+import { AvatarVariant, BadgeVariant } from 'support/types-interfaces-enums'
 
 Given('I render Advertise', () => {
     let author = ''
     let comment = ''
 
     generateMock('advertise-author', 'Name').then(e => {
-        author = e
+        author = typeof e === 'string' ? e : e.toString()
         generateMock('advertise-comment', 'Words')
-            .then(e => (comment = e))
+            .then(e => (comment = typeof e === 'string' ? e : e.toString()))
             .then(() => mount(<Advertise author={ author } comment={ comment } />))
     })
 })
 
 Given('I render Avatar with {string} preset', (preset: AvatarVariant) => {
-    generateMock('avatar-children', 'Letter').then(mock => {
+    generateMock('avatar-children', 'Letter', { length: 1 }).then(mock => {
         let params = {}
         switch (preset) {
             case 'primary':
@@ -34,4 +35,45 @@ Given('I render Avatar with {string} preset', (preset: AvatarVariant) => {
         }
         mount(<Avatar { ...params } />)
     })
+})
+
+Given('I render Badge with {string} preset', (preset: BadgeVariant) => {
+    const onClickSpy = generateSpy('badge-children')
+    let params: BadgeProps
+    let counter: number
+    generateMock('badge-counter', 'Number', { min: 1, max: 99 }).then(
+        mockCounter => {
+            counter = Number(mockCounter)
+            generateMock('badge-children', 'JSXButton', {
+                onClick: onClickSpy
+            }).then(mockChildren => {
+                switch (preset) {
+                    case 'primary':
+                        params = { color: 'primary', children: 'Badge', counter }
+                        break
+
+                    case 'secondary':
+                        params = {
+                            counter,
+                            color: 'secondary',
+                            children: mockChildren
+                        }
+                        break
+                    case 'with-dot':
+                        params = {
+                            counter,
+                            variant: 'dot',
+                            color: 'primary',
+                            children: 'Badge'
+                        }
+                        break
+                    default:
+                        params = { children: 'Badge', counter }
+                        break
+                }
+                mount(<Badge { ...params } />)
+            })
+
+        }
+    )
 })
