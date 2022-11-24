588c3f2fb7fe6340e242ad36bc86b0bf3a5e3b1d
Henrique Souza <henrique.asouza@outlook.com>
2022-11-24 11:17:39 -0300

    feat(cypress/integration/then): create I expect {string} spy to have been called times and I should see BadgeDot assertions

--- cypress/integration/common/Then.cy.tsx
+++ cypress/integration/common/Then.cy.tsx
@@ -1,5 +1,9 @@
 import { Then } from 'cypress-cucumber-preprocessor/steps'
-import { MockCats } from 'support/types-interfaces-enums'
+import {
+  MockCats,
+  MuiSelectors,
+  SpyCats
+} from '../../support/types-interfaces-enums'
 
 Then('I should see {string}', (text: string) =>
     cy.waitUntil(() => cy.contains(text).should('exist'))
@@ -24,3 +28,16 @@ Then(
   Then('I should not see any {string}', (element: string) => {
     cy.get(element).should('not.exist')
   })
+
+  Then(
+    'I expect {string} spy to have been called {int} times',
+    (spyName: SpyCats, quantity: number) => {
+      cy.getSpy(spyName).should('have.been.called', quantity)
+    }
+  )
+
+  Then('I should see {int} BadgeDot', (quantity: number) =>
+  cy.get(MuiSelectors.BadgeDot).then(elements => {
+    expect(elements).to.length(quantity)
+  })
+)
