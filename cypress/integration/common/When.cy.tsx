91919089d0e914b31b973af3bd63fc417bf91d4e
Henrique Souza <henrique.asouza@outlook.com>
2022-11-25 09:48:11 -0300

    feat(cypress/integration/when): create I click on text assertion

--- cypress/integration/common/When.cy.tsx
+++ cypress/integration/common/When.cy.tsx
@@ -1,5 +1,7 @@
 import { When } from 'cypress-cucumber-preprocessor/steps'
 
 When('I click on button {string}', (button: string) =>
-  cy.get(`button[id=${button}]`).click({ force: true })
+    cy.get(`button[id=${button}]`).click({ force: true })
 )
+
+When('I click on text {string}', (text: string) => cy.contains(text).click())
