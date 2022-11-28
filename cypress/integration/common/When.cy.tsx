be2df19321d52b192fcb2cf7a10f920d5da831fe
Henrique Souza <henrique.asouza@outlook.com>
2022-11-28 08:50:30 -0300

    feat(cypress/integration/given): create assertions
    I click on {int}th button

--- cypress/integration/common/When.cy.tsx
+++ cypress/integration/common/When.cy.tsx
@@ -4,4 +4,11 @@ When('I click on button {string}', (button: string) =>
     cy.get(`button[id=${button}]`).click({ force: true })
 )
 
+When('I click on {int}th button', (pos: number) =>
+    cy
+        .get('button')
+        .eq(pos - 1)
+        .click({ force: true })
+)
+
 When('I click on text {string}', (text: string) => cy.contains(text).click())
