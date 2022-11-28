163514ac6f70224b36e6884cfd8cd23d21ea0d2a
Henrique Souza <henrique.asouza@outlook.com>
2022-11-28 15:24:54 -0300

    feat(cypress/support): create checkbox factory

---
+++ cypress/support/factories/checkbox.tsx
@@ -0,0 +1,32 @@
+import { mount } from 'cypress/react'
+import React from 'react'
+import { CheckboxProps } from 'src/core/Checkbox'
+import { Checkbox } from '../../../src'
+// eslint-disable-next-line max-len
+import { checkboxPropsGenerator } from '../../support/utils/generators/checkbox-props'
+import { generateMockList, generateSpy } from '../component'
+import { CheckboxVariant } from '../types-interfaces-enums'
+
+export const CheckboxFactory = (variant: CheckboxVariant) => {
+    const onChangeSpy = generateSpy('checkbox-onchange')
+    const onHelperClickSpy = generateSpy('checkbox-onHelper-click')
+    generateMockList({
+        value: 'checkbox-params',
+        type: ['Word', 'CheckboxParams']
+    }).then(mockedList => {
+        const defaultProps = {
+            ...mockedList[1],
+            label: mockedList[0]
+        } as unknown as CheckboxProps
+
+        const props = checkboxPropsGenerator(
+            variant,
+            defaultProps,
+            onHelperClickSpy
+        )
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        mount(<Checkbox {...props} onChange={onChangeSpy} />)
+    })
+}
