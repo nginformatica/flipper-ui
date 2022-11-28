e2289f568901449749550761925b2953bb7da762
Henrique Souza <henrique.asouza@outlook.com>
2022-11-28 15:24:18 -0300

    feat(cypress/validator): create generic validator

--- cypress/support/utils/validators.ts
+++ cypress/support/utils/validators.ts
@@ -1,7 +1,20 @@
-import { ButtonVariant, MockTypes } from '../types-interfaces-enums'
+import {
+    ButtonVariant,
+    CheckboxVariant,
+    MockTypes
+} from '../types-interfaces-enums'
 
 export const mockValidators = (type: MockTypes) => (value: MockTypes) =>
     value === type
+
+export const checkboxValidators =
+    (type: CheckboxVariant) => (value: CheckboxVariant) =>
+        value === type
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+export function validator<T>(type: T) {
+    return (value: T) => value === type
+}
 
 export const buttonValidators =
     (variant: ButtonVariant) => (value: ButtonVariant) =>
