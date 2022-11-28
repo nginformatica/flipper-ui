e8ff9c32babf3bf222638feb0aa90b24e01d3862
Henrique Souza <henrique.asouza@outlook.com>
2022-11-28 15:25:34 -0300

    feat(cypress/integration/when): create assertions
    I click on {int}th checkbox, I hover mocked text {string}

--- cypress/integration/common/When.cy.tsx
+++ cypress/integration/common/When.cy.tsx
@@ -1,4 +1,5 @@
 import { When } from 'cypress-cucumber-preprocessor/steps'
+import { MockCats } from 'cypress/support/types-interfaces-enums'
 
 When('I click on button {string}', (button: string) =>
     cy.get(`button[id=${button}]`).click({ force: true })
@@ -11,4 +12,31 @@ When('I click on {int}th button', (pos: number) =>
         .click({ force: true })
 )
 
+When('I click on {int}th checkbox', (pos: number) =>
+    cy
+        .get('input[type=checkbox]')
+        .eq(pos - 1)
+        .click({ force: true })
+)
+
 When('I click on text {string}', (text: string) => cy.contains(text).click())
+
+When('I hover mocked text {string}', (cat: MockCats) => {
+    cy.getMock(cat).then(mock => {
+        if (mock instanceof Array) {
+            mock.forEach(item => {
+                if (typeof item === 'string') {
+                    cy.contains(item).realHover()
+
+                    return
+                }
+            })
+        }
+
+        if (typeof mock === 'string') {
+            cy.contains(mock).realHover()
+        }
+        console.log({ mock })
+    })
+    // cy.contains(text).realHover()
+})
