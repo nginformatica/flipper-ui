36187ee8b6875a7f0e4e0116a059f89514795a62
Henrique Souza <henrique.asouza@outlook.com>
2022-11-29 11:45:54 -0300

    feat(cypress/factories): create ContainerFactory

---
+++ cypress/support/factories/container.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { mount } from 'cypress/react'
+import { Container } from '../../../src'
+import { generateMock } from '../component'
+
+export const ContainerFactory = () => {
+    generateMock({ value: 'container-content', type: 'Words' }).then(
+        mockedWords => {
+            generateMock({
+                value: 'container-style',
+                type: 'GenericStyleParams'
+            }).then(props => {
+                console.log({ props })
+                if (props instanceof Object) {
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore
+                    mount(<Container {...props}>{mockedWords}</Container>)
+                }
+            })
+        }
+    )
+}
