b9e462f53ba9a233ba1ddcc8e35c3472b01899a0
Henrique Souza <henrique.asouza@outlook.com>
2022-12-21 18:27:26 -0300

    test: ensure Header can spread props, specialy data-cy

--- cypress/support/factories/header.tsx
+++ cypress/support/factories/header.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { mount } from 'cypress/react'
 import { Header } from '../../../src'
-import { generateMock } from '../component'
+import { generateTypedMock } from '../component'
 import { Generators } from '../'
 import { HeaderVariant } from '../types-interfaces-enums'
 
 export const HeaderFactory = (preset: HeaderVariant) => {
-    generateMock({ value: 'header-content', type: 'Word' }).then(mockedWord => {
-        const props = Generators.headerPropsGenerator(preset)
-        mount(<Header {...props}>{mockedWord}</Header>)
-    })
+    generateTypedMock<string>({ value: 'header-content', type: 'Word' }).then(
+        mockedWord => {
+            const props = Generators.headerPropsGenerator(preset)
+            mount(
+                <Header data-cy='header-container' {...props}>
+                    {mockedWord}
+                </Header>
+            )
+        }
+    )
 }
