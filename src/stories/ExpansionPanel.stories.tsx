fadea00e386adecd4ce92da00aa9cf007ef7cd8e
Henrique Souza <henrique.asouza@outlook.com>
2023-01-23 14:24:33 -0300

    doc(ExpansionPanel): editable variant storybook documentation

--- src/stories/ExpansionPanel.stories.tsx
+++ src/stories/ExpansionPanel.stories.tsx
@@ -4,15 +4,62 @@ import ExpansionPanel from '../core/ExpansionPanel'
 import Typography from '../core/Typography'
 import ExpandMore from '@material-ui/icons/ExpandMore'
 import Button from '../core/Button'
+import TextField from '../core/TextField'
 
 export default {
     title: 'ExpansionPanel',
     component: ExpansionPanel
 } as ComponentMeta<typeof ExpansionPanel>
 
-const Template: ComponentStory<typeof ExpansionPanel> = args => (
-    <ExpansionPanel {...args} />
-)
+const Template: ComponentStory<typeof ExpansionPanel> = args => {
+    const [editing, setEditing] = React.useState(args.editing)
+    const [details, setDetails] = React.useState(args.details)
+    const inputRef = React.useRef<HTMLInputElement>(null)
+
+    const handleEditClick = () => {
+        setEditing(true)
+    }
+
+    const handleSaveClick = () => {
+        setDetails(inputRef.current?.value)
+        setEditing(false)
+    }
+
+    const handleKeyDown = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            handleSaveClick()
+        }
+
+        if (event.key === 'Escape') {
+            setEditing(false)
+        }
+    }
+
+    const renderDetails = () => {
+        if (editing) {
+            return (
+                <TextField
+                    autoFocus
+                    inputRef={inputRef}
+                    onKeyDown={handleKeyDown}
+                />
+            )
+        }
+
+        return details
+    }
+
+    return (
+        <ExpansionPanel
+            {...args}
+            editing={editing}
+            editStyle={{ backgroundColor: 'red', width: '40px' }}
+            onEditClick={handleEditClick}
+            onSaveClick={handleSaveClick}
+            details={renderDetails()}
+        />
+    )
+}
 export const Default = Template.bind({})
 
 Default.args = {
@@ -40,5 +87,14 @@ Disabled.args = {
     disabled: true,
     summary: 'Dormammu',
     details: 'I have come to bargain',
+    expandIcon: <ExpandMore />
+}
+
+export const WithEditable = Template.bind({})
+
+WithEditable.args = {
+    editable: true,
+    summary: 'Editable Expansion Panel',
+    details: 'This is an editable expansion panel',
     expandIcon: <ExpandMore />
 }
