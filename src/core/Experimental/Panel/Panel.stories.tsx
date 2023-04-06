08c0389a2709b447d71dceec7d979f8979784b1a
Henrique Souza <henrique.asouza@outlook.com>
2023-04-06 13:33:53 -0300

    feat(panel): import component from ngi-core

---
+++ src/core/Experimental/Panel/Panel.stories.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import PanelComponent from './Panel'
+
+export const Panel = () => {
+    const [open, setOpen] = React.useState(true)
+
+    const handleClickSummary = () => {
+        setOpen(!open)
+    }
+
+    return (
+        <PanelComponent
+            expanded={open}
+            onClick={handleClickSummary}
+            title='Foo'
+            details={<button onClick={() => setOpen(false)}>bar</button>}
+        />
+    )
+}
+
+export const NestedPanel = () => {
+    const [open, setOpen] = React.useState(true)
+
+    const handleClickSummary = () => {
+        setOpen(!open)
+    }
+
+    return (
+        <PanelComponent
+            nested
+            expanded={open}
+            onClick={handleClickSummary}
+            title='Foo'
+            details={<button onClick={() => setOpen(false)}>bar</button>}
+        />
+    )
+}
+
+export const WithOmittedIcon = () => {
+    const [open, setOpen] = React.useState(true)
+
+    const handleClickSummary = () => {
+        setOpen(!open)
+    }
+
+    return (
+        <PanelComponent
+            expanded={open}
+            hideExpansionIcon={true}
+            onClick={handleClickSummary}
+            title='Foo'
+            details={<button onClick={() => setOpen(false)}>bar</button>}
+        />
+    )
+}
+
+export default {
+    title: 'Experimental/Panel',
+    component: Panel
+}
