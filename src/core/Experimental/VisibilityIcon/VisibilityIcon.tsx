78e830d89cfbf8bd262b207d70bd6ee84dba30f6
Henrique Souza <henrique.asouza@outlook.com>
2023-04-06 15:14:19 -0300

    feat(visibility-icon): import component from ngi-core

---
+++ src/core/Experimental/VisibilityIcon/VisibilityIcon.tsx
@@ -0,0 +1,30 @@
+import IconButton from '../../IconButton'
+import {
+    Visibility as IconVisibility,
+    VisibilityOff as IconVisibilityOff
+} from '../../../icons'
+import React from 'react'
+import styled from 'styled-components'
+
+interface IProps {
+    name?: string
+    show: boolean
+    onToggle(): void
+}
+
+const StyledButton = styled(IconButton)`
+    width: 36px !important;
+    height: 36px !important;
+`
+
+const VisibilityIcon = ({ show, onToggle, name, ...otherProps }: IProps) => (
+    <StyledButton {...otherProps} padding='0px' name={name} onClick={onToggle}>
+        {show ? (
+            <IconVisibilityOff role='icon-off' />
+        ) : (
+            <IconVisibility role='icon-on' />
+        )}
+    </StyledButton>
+)
+
+export default VisibilityIcon
---
+++ src/core/Experimental/VisibilityIcon/index.tsx
@@ -0,0 +1,1 @@
+export { default as VisibilityIcon } from './VisibilityIcon'
