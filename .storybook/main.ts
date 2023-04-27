5667e6bb55043eec68bcffb813240d6729729321
Henrique Souza <henrique.asouza@outlook.com>
2023-04-27 08:26:22 -0300

    chore: update typescript and remove unused libs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,30 +1,40 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
-import path from 'path';
+import type { StorybookConfig } from '@storybook/react-webpack5'
+import path from 'path'
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
-  docs: {
-    autodocs: true
-  },
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {}
-  },
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      shouldRemoveUndefinedFromOptional: true,
-      propFilter: prop => prop.parent ? !/node_modules\/(?!@mui)/.test(prop.parent.fileName) : true
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)'
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions'
+    ],
+    docs: {
+        autodocs: true
+    },
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {}
+    },
+    typescript: {
+        check: false,
+        checkOptions: {},
+        reactDocgen: 'react-docgen-typescript',
+        reactDocgenTypescriptOptions: {
+            shouldExtractLiteralValuesFromEnum: true,
+            shouldRemoveUndefinedFromOptional: true,
+            propFilter: prop =>
+                prop.parent
+                    ? !/node_modules\/(?!@mui)/.test(prop.parent.fileName)
+                    : true
+        }
+    },
+    webpackFinal: async config => {
+        if (config.resolve?.alias) {
+            config.resolve.alias['@'] = path.resolve(__dirname, '../src')
+        }
+        return config
     }
-  },
-  webpackFinal: async config => {
-    if (config.resolve?.alias) {
-      config.resolve.alias['@'] = path.resolve(__dirname, '../src');
-    }
-    return config;
-  }
-};
-export default config;
+}
+export default config
