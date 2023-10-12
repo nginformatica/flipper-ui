860e0197f67db0452f6feb6cb96cbe44aa1aedca
Heder Alves <alvesheder92@gmail.com>
2023-10-12 14:08:56 -0300

    Fix: migrate new typing functions and component

--- src/core/inputs/mask-field/index.tsx
+++ src/core/inputs/mask-field/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import TextField, { TextFieldProps } from '@/core/inputs/text-field'
-import NumberFormat, { NumberFormatProps } from 'react-number-format'
+import TextField from '@/core/inputs/text-field'
+import { NumericFormatProps, NumberFormatBase } from 'react-number-format'
+import { InputProps } from '@material-ui/core'
+import { FormatInputValueFunction } from 'react-number-format/types/types'
 
-export interface MaskFieldProps extends NumberFormatProps {
+// Interface para as propriedades de entrada específicas do MaskField
+interface MaskFieldInputProps extends InputProps {
+    // Adicione aqui quaisquer propriedades adicionais necessárias para o TextField
+}
+
+export interface MaskFieldProps extends NumericFormatProps {
     mask?: string
     type?: 'text' | 'tel' | 'password'
     decimalSeparator?: string
-    format?: string
+    format?: string | FormatInputValueFunction
     decimalScale?: number
     thousandSeparator?: boolean | string
     fixedDecimalScale?: boolean
-    customInput?: React.ComponentType<TextFieldProps>
+    color?: 'primary' | 'secondary' | undefined
+    // eslint-disable-next-line max-len
+    customInput?: React.ComponentType<MaskFieldInputProps> // Use a nova interface aqui
 }
 
 export const MaskField = (props: MaskFieldProps) => {
@@ -19,10 +28,18 @@ export const MaskField = (props: MaskFieldProps) => {
     return (
         // Although react-number-format allow use of additional props,
         // shows problem with some props like have been do this
-        // actually on flipper-ui. (e.g. errors treatment)
-        <NumberFormat
+        // actually on flipper-ui. (e.g. errors treatment
+        <NumberFormatBase
             {...otherProps}
-            customInput={customInput || TextField}></NumberFormat>
+            customInput={
+                customInput ||
+                (TextField as React.ComponentType<MaskFieldInputProps>)
+            }
+            format={
+                typeof otherProps.format === 'string'
+                    ? undefined
+                    : otherProps.format
+            }></NumberFormatBase>
     )
 }
 
