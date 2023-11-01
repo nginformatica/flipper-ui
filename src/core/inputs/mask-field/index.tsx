8a9655b55135afa8d0d86c01189dec04e397a086
Heder Alves <alvesheder92@gmail.com>
2023-10-31 21:28:14 -0300

    chore: add and organize typings

--- src/core/inputs/mask-field/index.tsx
+++ src/core/inputs/mask-field/index.tsx
@@ -1,38 +1,55 @@
 import React, { ChangeEvent } from 'react'
-import TextField from '@/core/inputs/text-field'
+import TextField, { IOption } from '@/core/inputs/text-field'
 import { NumericFormatProps, NumberFormatBase } from 'react-number-format'
 import { FormatInputValueFunction } from 'react-number-format/types/types'
 
 export interface MaskFieldProps extends NumericFormatProps {
-    children?: React.ReactNode
     error?: boolean
-    value?: number | string
-    style?: React.CSSProperties
-    margin?: string
-    id?: string
+    showEdit?: boolean
+    hasClear?: boolean
     autoFocus?: boolean
+    multiline?: boolean
+    select?: boolean
     fullWidth?: boolean
     required?: boolean
     disabled?: boolean
+    fixedDecimalScale?: boolean
+    thousandSeparator?: boolean | string
+    margin?: string
+    id?: string
+    name?: string
+    mask?: string
+    placeholder?: string
+    label?: string
+    autoComplete?: string
+    decimalSeparator?: string
+    format?: string | FormatInputValueFunction
+    defaultValue?: string | number
+    value?: string | number
+    rows?: string | number
+    rowsMax?: string | number
+    decimalScale?: number
+    SelectProps?: object
     InputProps?: object
     inputProps?: object
     InputLabelProps?: object
-    placeholder?: string
-    label?: string
-    defaultValue?: string | number
-    name?: string
-    mask?: string
+    options?: IOption[] | string
+    children?: React.ReactNode
+    helperText?: React.ReactNode
+    helperIcon?: React.ReactNode
+    style?: React.CSSProperties
+    inputRef?: React.Ref<HTMLInputElement>
     type?: 'text' | 'tel' | 'password'
-    decimalSeparator?: string
-    format?: string | FormatInputValueFunction
-    decimalScale?: number
-    thousandSeparator?: boolean | string
-    fixedDecimalScale?: boolean
+    variant?: 'standard' | 'outlined' | 'filled'
     color?: 'primary' | 'secondary' | undefined
+    customInput?: React.ComponentType
+    onClear?: () => void
     onBlur?: () => void
     onHelperClick?: () => void
     onChange?: (event: ChangeEvent<HTMLInputElement>) => void
-    customInput?: React.ComponentType
+    onKeyUp?: (event: React.KeyboardEvent<HTMLInputElement>) => void
+    onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void
+    onPaste?: (event: React.ClipboardEvent<HTMLInputElement>) => void
 }
 
 export const MaskField = (props: MaskFieldProps) => {
