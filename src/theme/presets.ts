c822206d497d26d9c41f03a2f8481402c1ccd88f
Karine Brandelli Padilha <karine.padilha@ngi.com.br>
2024-01-02 10:10:49 -0300

    feat: migrate styleguide to flipper-ui

---
+++ src/theme/presets.ts
@@ -0,0 +1,34 @@
+export enum Feedback {
+    danger = 'danger',
+    warning = 'warning',
+    success = 'success',
+    info = 'info'
+}
+
+export enum Priority {
+    critical = 'critical',
+    high = 'high',
+    medium = 'medium',
+    low = 'low',
+    minimal = 'minimal',
+    neutral = 'neutral'
+}
+
+export enum Criticality {
+    high = 'high',
+    medium = 'medium',
+    low = 'low'
+}
+
+export enum Graphically {
+    pink = 'pink',
+    purple = 'purple',
+    brown = 'brown',
+    red = 'red',
+    orange = 'orange',
+    yellow = 'yellow',
+    lightgreen = 'lightgreen',
+    green = 'green',
+    blue = 'blue',
+    gray = 'gray'
+}
