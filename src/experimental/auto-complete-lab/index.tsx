910a44f57a4443fc24488c6fe66938e974dd43f6
Karine Brandelli Padilha <karine.padilha@ngi.com.br>
2024-01-04 17:20:07 -0300

    chore: replace @mui for @material/lab

--- src/experimental/auto-complete-lab/index.tsx
+++ src/experimental/auto-complete-lab/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
-import { Autocomplete as MuiAutocomplete } from '@mui/material'
 import { makeStyles } from '@material-ui/core/styles'
+import { default as MuiAutocomplete } from '@material-ui/lab/Autocomplete'
+export {
+    AutocompleteRenderInputParams,
+    AutocompleteProps,
+    AutocompleteCloseReason,
+    AutocompleteChangeReason,
+    AutocompleteInputChangeReason,
+    AutocompleteGetTagProps,
+    AutocompleteRenderOptionState
+} from '@material-ui/lab/Autocomplete'
 
 const useStyles = makeStyles({
     inputRoot: {
