b64a3ff258559d7ae96e9b919ea770f508432f2b
Karine Brandelli <karinebrandelli@live.com>
2024-02-18 10:21:37 -0300

    chore: disable max-len

--- src/core/inputs/date-time/index.tsx
+++ src/core/inputs/date-time/index.tsx
@@ -9,6 +9,7 @@ import {
 } from '@material-ui/pickers'
 import type { DefaultProps } from '../../types'
 import type { KeyboardDatePickerProps } from '@material-ui/pickers/DatePicker'
+// eslint-disable-next-line @stylistic/max-len
 import type { KeyboardDateTimePickerProps } from '@material-ui/pickers/DateTimePicker'
 import type { KeyboardTimePickerProps } from '@material-ui/pickers/TimePicker'
 import type { MaterialUiPickersDate } from '@material-ui/pickers/typings/date'
