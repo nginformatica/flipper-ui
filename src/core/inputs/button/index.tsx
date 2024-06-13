0aeaef69ebb36f5bd0a8ebd4b1039aa6e9ee4901
Karine Brandelli <karinebrandelli@live.com>
2024-06-13 16:29:33 -0300

    feat: migrate Button from @material to @mui

--- src/core/inputs/button/index.tsx
+++ src/core/inputs/button/index.tsx
@@ -1,42 +1,56 @@
 import React from 'react'
 import type { ElementType, MouseEvent, ReactNode } from 'react'
-import { Button as MuiButton } from '@material-ui/core'
+import MuiButton from '@mui/material/Button'
 import type { DefaultProps } from '../../types'
+import type { ButtonProps } from '@mui/material/Button'
 
-export interface ButtonProps extends DefaultProps {
-    disabled?: boolean
-    selected?: boolean
-    disableTouchRipple?: boolean
-    component?: ElementType
-    color?: 'default' | 'primary' | 'inherit' | 'secondary'
-    size?: 'small' | 'medium' | 'large'
-    href?: string
-    fullWidth?: boolean
-    variant?: 'text' | 'outlined' | 'contained' | 'dashed'
-    target?: string
+export interface IButtonProps extends DefaultProps, ButtonProps {
     children?: ReactNode
+    color?:
+        | 'inherit'
+        | 'primary'
+        | 'secondary'
+        | 'success'
+        | 'error'
+        | 'info'
+        | 'warning'
+    component?: ElementType
+    dashed?: boolean
+    disabled?: boolean
+    disableTouchRipple?: boolean
     'data-testid'?: string
+    fullWidth?: boolean
+    href?: string
+    selected?: boolean
+    size?: 'small' | 'medium' | 'large'
+    target?: string
+    variant?: 'text' | 'outlined' | 'contained'
     onClick?(event: MouseEvent<HTMLButtonElement>): void
 }
 
 const Button = ({
     children,
+    color = 'inherit',
+    dashed,
+    href = '',
     margin,
     padding,
     style = {},
     variant,
     ...otherProps
-}: ButtonProps) => (
+}: IButtonProps) => (
     <MuiButton
-        {...otherProps}
-        variant={variant === 'dashed' ? 'outlined' : variant}
+        href={href}
+        color={color}
+        variant={dashed ? 'outlined' : variant}
         style={{
             ...style,
             margin,
             padding,
             opacity: otherProps.selected ? 0.5 : 1,
-            borderStyle: variant
-        }}>
+            borderStyle: dashed ? 'dashed' : variant
+        }}
+        {...otherProps}>
         {children}
     </MuiButton>
 )
