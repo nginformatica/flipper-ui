14d6bea76afc7ca20314dbac98150d0d6d6f9462
Karine Brandelli <karinebrandelli@live.com>
2024-06-14 17:20:25 -0300

    feat: migrate Slider from @material to @mui

--- src/core/inputs/slider/index.tsx
+++ src/core/inputs/slider/index.tsx
@@ -1,28 +1,58 @@
 import React from 'react'
-import MuiSlider from '@material-ui/core/Slider'
+import MuiSlider from '@mui/material/Slider'
+import { makeStyles } from '@mui/styles'
 import type { DefaultProps } from '@/core/types'
-import type { SliderProps } from '@material-ui/core/Slider'
+import type { SliderProps } from '@mui/material/Slider'
+import { theme } from '@/theme'
 
-export interface ISliderProps {
+export interface ISliderProps
+    extends Omit<SliderProps, 'defaultValue'>,
+        DefaultProps {
     defaultValue?: number | number[]
+    color?: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning'
 }
 
-export type FlipperSliderProps = Omit<SliderProps, 'defaultValue'> &
-    DefaultProps &
-    ISliderProps
+type TProps = {
+    color?: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning'
+}
+
+const labels = {
+    primary: theme.colors.primary.main,
+    secondary: theme.colors.secondary.main,
+    error: theme.colors.feedback.danger,
+    info: theme.colors.feedback.info,
+    success: theme.colors.feedback.success,
+    warning: theme.colors.feedback.warning
+}
+
+const useStyles = makeStyles(() => ({
+    valueLabel: (props: TProps) => ({
+        backgroundColor: labels[props.color || 'primary'],
+        borderRadius: '5px'
+    })
+}))
 
 const Slider = ({
     padding,
     margin,
+    size,
+    color = 'primary',
     style = {},
     defaultValue,
     ...otherProps
-}: FlipperSliderProps) => (
-    <MuiSlider
-        {...otherProps}
-        defaultValue={defaultValue}
-        style={{ padding, margin, ...style }}
-    />
-)
+}: ISliderProps) => {
+    const classes = useStyles({ color })
+
+    return (
+        <MuiSlider
+            {...otherProps}
+            size={size || 'small'}
+            color={color}
+            classes={{ valueLabel: classes.valueLabel }}
+            defaultValue={defaultValue}
+            style={{ padding, margin, ...style }}
+        />
+    )
+}
 
 export default Slider
